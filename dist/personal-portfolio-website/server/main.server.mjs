df31f28de8df9d64d31d7c880765113343cd55de
Tirth Shah <shahti42@gmail.com>
2024-10-09 06:32:27 -0700

    added functionality for github pages

--- dist/personal-portfolio-website/server/main.server.mjs
+++ dist/personal-portfolio-website/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-2RJXH766.mjs";import"./chunk-XYHM6EFN.mjs";import"./chunk-YRSNHIOK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-JKI5CIQL.mjs";import"./chunk-XYHM6EFN.mjs";import"./chunk-YRSNHIOK.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
